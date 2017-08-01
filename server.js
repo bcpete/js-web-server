96f57e7a7a6996991cc42019a9d4ec6d58d8ed22
hometownzero <bradycpeters@gmail.com>
2017-08-01 18:38:56 -0500

    learning how to serve dynamically generated templates with express and the handlebars engine. added folder for views.

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express<h1>');
-    res.send({
-        name: 'Brady',
-        likes: [
-            'Food',
-            'Sleep',
-            'Code',
-            'Node'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome dood'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -27,6 +28,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
